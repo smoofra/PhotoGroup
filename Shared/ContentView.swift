e9f28f564b15e2a7205d40c1408c66a9afa9fa03
Lawrence D'Anna <larry@elder-gods.org>
2022-03-04 09:04:52 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Lawrence D'Anna on 3/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PhotoGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGroupApp.swift
+//  Shared
+//
+//  Created by Lawrence D'Anna on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
