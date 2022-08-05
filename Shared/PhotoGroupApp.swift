48b7819a3883c26f3725317879b83b1d031048cd
Lawrence D'Anna <larry@elder-gods.org>
2022-08-04 20:39:59 -0400

    wip

--- Shared/PhotoGroupApp.swift
+++ Shared/PhotoGroupApp.swift
@@ -26,15 +26,22 @@ struct PhotoGroupApp: App {
         }
     }
     
-    func writeCSV(path:String) {
+    func writeCSV(filename:String) {
 
-        FileManager.default.createFile(atPath: path,  contents:Data(), attributes: nil)
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        if documentsDirectories.count < 1 {
+            print("can't find Documents")
+        }
+        let documentsDirectory = documentsDirectories[0]
+        let path = documentsDirectory.appendingPathComponent(filename).path
+        print("writing csv to ", path)
+        FileManager.default.createFile(atPath: path, contents: Data(), attributes: nil)
         guard let f = FileHandle(forWritingAtPath: path) else {
-            print("can't open file");
+            print("can't open file:", path);
             return;
         }
 
-        f.write("id,creationDate,modificationDate,mediaType,mediaSubtypes,flags,filename,size,url\n".data(using: .utf8)!)
+        f.write("id,creationDate,modificationDate,mediaType,mediaSubtypes,flags,resourceType,filename,size,url\n".data(using: .utf8)!)
 
         let fetchResult = Photos.PHAsset.fetchAssets(with: PHFetchOptions())
 
@@ -52,15 +59,15 @@ struct PhotoGroupApp: App {
             for resource in PHAssetResource.assetResources(for: asset) {
                 
                 let flags = asset.isFavorite ? "❤️" : ""
-
-
-                let line = String(format: "%@,%@,%@,%d,%d,%@,%@,%d,%@\n",
+                
+                let line = String(format: "%@,%@,%@,%d,%d,%@,%d,%@,%d,%@\n",
                                   asset.localIdentifier,
                                   asset.creationDate?.ISO8601Format(style) ?? "",
                                   asset.modificationDate?.ISO8601Format(style) ?? "",
                                   asset.mediaType.rawValue,
                                   asset.mediaSubtypes.rawValue,
                                   flags,
+                                  resource.type.rawValue,
                                   quote(resource.originalFilename),
                                   resource_fileSize(resource),
                                   quote(resource_fileURL(resource)?.absoluteString ?? ""))
@@ -83,7 +90,7 @@ struct PhotoGroupApp: App {
             return
         }
         print ("==got authorization.")
-        self.writeCSV(path: "/tmp/assets.csv")
+        self.writeCSV(filename: "assets.csv")
     }
     
     init() {
